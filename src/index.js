81e4ab2abdc298ce6b6eb06777c97659b8f5d8d4
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-10-12 12:56:59 +0200

    DOCKERSETUP: WORKIGN
    Added docker compose, docker file and migrations

--- src/index.js
+++ src/index.js
@@ -19,39 +19,41 @@ app.get("/", (request,response ) =>{
 
 })
 
-const db = knex(knexfile.development);
+// const db = knex(knexfile.development);
 
-// Run pending migrations
-db.migrate.latest()
-  .then(() => {
-    // Migration succeeded; start the server
-    app.listen(port, (err) => {
-      if (!err) {
-        console.log("Server is running on port " + port);
-      } else {
-        console.log(err);
-      }
-    });
-  })
-  .catch((error) => {
-    console.error("Error running migrations:", error);
-    process.exit(1); // Exit the application on migration error
-  });
+// // Run pending migrations
+// db.migrate.latest()
+//   .then(() => {
+//     // Migration succeeded; start the server
+//     app.listen(port, (err) => {
+//       if (!err) {
+//         console.log("Server is running on port " + port);
+//       } else {
+//         console.log(err);
+//       }
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error running migrations:", error);
+//     process.exit(1); // Exit the application on migration error
+//   });
 
 
-db("schema_name.students").select("*")
+// db("schema_name.students").select("*")
 
-// Read all phones
-app.get("/api/phones", (request, response) => {
-  db("phones")
-    .select("*")
-    .then((phones) => {
-      response.json(phones);
-    })
-    .catch((error) => {
-      response.status(500).json({ error: "Error fetching phones" });
-    });
-});
+// // Read all phones
+// app.get("/api/phones", (request, response) => {
+//   db("phones")
+//     .select("*")
+//     .then((phones) => {
+//       response.json(phones);
+//     })
+//     .catch((error) => {
+//       response.status(500).json({ error: "Error fetching phones" });
+//     });
+// });
+
+
 
 
 
