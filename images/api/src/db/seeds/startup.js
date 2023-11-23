9099e31e5b8b953cd671bf1460df06242f0d3545
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2023-11-23 12:51:29 +0100

    "Migration: Added new table called brands"

--- images/api/src/db/seeds/startup.js
+++ images/api/src/db/seeds/startup.js
@@ -1,14 +1,21 @@
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  
-  knex("phones").select("*").then((data) => {
-    if(!data[0]) {
+exports.seed = async function (knex) {
+  // Check if the phones table is empty
+  const isEmpty = await knex("phones").select("*").first();
+
+  if (!isEmpty) {
+    // Do nothing if the table is not empty
+    return Promise.resolve();
+  }
+
+  // Deletes ALL existing entries since the table is empty
+  return knex("phones")
+    .del()
+    .then(function () {
+      // Inserts seed entries
       return knex("phones").insert([
-        { name: "Phone 1", brand: "Brand A" },
-        { name: "Phone 2", brand: "Brand B" },
+        { phone_model: "Test", brand_id: 1 }, // Assuming Apple has an id of 1
+        { phone_model: "Phone 2", brand_id: 2 }, // Assuming Samsung has an id of 2
         // Add more seed data as needed
       ]);
-    }
-  })
+    });
 };
-
