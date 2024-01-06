c3e2f5c90e6300a22857ed2d7fe75d50fdfe9794
AndyHuizenga <56120771+AndyHuizenga@users.noreply.github.com>
2024-01-06 13:26:19 +0100

    Build(Featlogin): builed Login page

--- images/my-react-app/src/Login.js
+++ images/my-react-app/src/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -9,18 +10,32 @@ const Login = () => {
     console.log('Login button clicked');
   };
 
+  const handleNewUser = () => {
+    // Implement your login logic here
+    console.log('Login button clicked');
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <h2>Login</h2>
+          <form>
+            <div className="mb-3 mt-5">
+              <label htmlFor="email" className="form-label" style={{ float: 'left' }}>Email:</label>
+              <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
 
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label" style={{ float: 'left' }}>Password:</label>
+              <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
 
-        <button type="button" onClick={handleLogin}>Login</button>
-      </form>
+            <button type="button" className="btn btn-primary" onClick={handleLogin}>Login</button>
+            <button type="button" className="btn btn-primary"style={{ marginLeft: '10px' }}  onClick={handleNewUser}>New User</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
